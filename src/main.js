8df81a0fb8b45b6d740c9df5ed6b5bf3d3e01846
Tej Hirpara <79459390+Tej-2407@users.noreply.github.com>
2022-04-10 10:47:14 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import Antd from 'ant-design-vue';
 import App from './App';
 import 'ant-design-vue/dist/antd.css';
 import router from './router';
-// import { API_TOKEN } from "./config/env"
 
 const app = createApp(App);
 
